a7b8f4246d2bd82d1896e4559be63db6271d01e6
Celestino <celestinojones1995@yahoo.com>
2019-05-23 16:37:48 -0700

    LinkedLists5

--- ChangingtheLine.js
+++ ChangingtheLine.js
@@ -124,12 +124,157 @@ function removeVal(list, val)
     return list;
 }
 
-Display(myList);
-prependVal(myList, 1, secondNode);
-Display(myList);
-
+prependVal(myList, 444, secondNode);
 appendVal(myList, 30, thirdNode);
+removeVal(myList, 42);
+appendVal(myList, 1, thirdNode);
+
+/*Linked Lists Algorithms - Set 5
+SList: Move Min to Front
+Create a standalone function that locates the minimum value in a linked list, and moves that node to the front of the list. Return the new list, with all nodes still present, and all (except for the new head node) in their original order.
+
+SList: Move Max to Back
+Create a standalone function that locates the maximum value in a linked list, and moves that node to the back of the list. Return the new list, with all nodes still present, and all in their original order except for the node you moved to the end of the singly linked list.
+
+SList: Swap Nodes
+Create swapNodes(list, node1, node2). Locate node1 within the list given and swap with node2. Return the modified list.
+
+Bonus: what would you do if node1 or node2 does not exist in your list?*/
+
+function minToFront(list)
+{
+    var currMinBefore = null;
+    var currMin = list.head;
+    var runner = list.head;
+    while(runner.next != null)
+    {
+        if(runner.next.val < currMin.val)
+        {
+            currMinBefore = runner;
+            currMin = runner.next;
+        }
+        runner = runner.next;
+    }
+
+    if(currMinBefore == null) //if the min value is already at the front
+        return list;
+
+    currMinBefore.next = currMin.next;
+    var currHead = list.head;
+    list.head = currMin;
+    currMin.next = currHead;
+    return list;
+}
+
+function maxToBack(list)
+{
+    var currMaxBefore = null;
+    var currMax = list.head;
+    var runner = list.head;
+    while(runner.next != null)
+    {
+        if(runner.next.val > currMax.val)
+        {
+            currMaxBefore = runner;
+            currMax = runner.next;
+        }
+        runner = runner.next;
+
+        if(runner.next == null) // we're at the end now so we will make our changes
+        {
+            if(runner == currMax) //if the node at the end is the max value
+                return list;
+            if(currMaxBefore == null) //if we found no cases where anything was greater than currMax.val (the head)
+            {
+                runner.next = currMax;
+                return list;
+            }
+
+            currMaxBefore.next = currMax.next;
+            runner.next = currMax;
+            currMax.next = null;
+            break;
+        }
+    }
+    
+    return list;
+}
+
+function swapNodes(list, node1, node2)
+{
+    var preSwitch1 = null;
+    var switch1 = null;
+    
+    var preSwitch2 = null;
+    var switch2 = null;
+
+    var runner = list.head;
+    while(runner.next != null)
+    {
+        if(runner.next == node1)
+        {
+            preSwitch1 = runner;
+            switch1 = runner.next;
+        }
+        if(runner.next == node2)
+        {
+            preSwitch2 = runner;
+            switch2 = runner.next;
+        }
+
+        runner = runner.next;
+    }
+    if(switch1 == null || switch2 == null)
+    {
+        console.log("Couldn't find one of the nodes you wanted to switch in this list. Are you sure they're here?");
+        return list;
+    }
+    //create new heads if the node theyre switching with is a head
+    if(switch1 == list.head)
+        list.head = switch2;
+    if(switch2 == list.head)
+        list.head = switch1;
+    if(switch1.next == switch2 || switch2.next == switch1)
+    {
+        if(switch1.next == switch2)
+        {
+            var switch2Pointer = switch2.next;
+            switch2.next = switch1;
+            switch1.next = switch2Pointer;
+            preSwitch1.next = switch2;
+            return list;
+        }
+        if(switch2.next == switch1)
+        {
+            var switch1Pointer = switch1.next;
+            switch1.next = switch2;
+            switch2.next = switch1Pointer;
+            preSwitch2.next = switch1;
+            return list;
+        }
+    }
+    //continue here if switch1 and switch2 are not directly next to each other
+
+    preSwitch2.next = switch1;
+    preSwitch1.next = switch2;
+    
+    var switch1Pointer = switch1.next;
+    switch1.next = switch2.next;
+    switch2.next = switch1Pointer;
+    return list;
+}
+
+Display(myList);
+minToFront(myList);
+Display(myList);
+appendVal(myList, 842, myList.head);
+Display(myList);
+maxToBack(myList);
 Display(myList);
 
-removeVal(myList, 42);
+console.log("switching nodes");
 Display(myList);
+swapNodes(myList, secondNode, thirdNode);
+Display(myList);
+
+swapNodes(myList, firstNode, thirdNode);
